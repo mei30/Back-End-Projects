18509691c944d92b964b4fbe2f50a483b97cbb6c
Mei30 <meisamjafarimosleh@gmail.com>
2023-02-15 13:31:12 +0330

    make server configrable

--- task-management/src/auth/jwt-strategy.ts
+++ task-management/src/auth/jwt-strategy.ts
@@ -5,12 +5,16 @@ import { Strategy, ExtractJwt } from 'passport-jwt';
 import { UsersRepository } from './users.repository';
 import { JwtPayload } from './jwt-payload.interface';
 import { User } from './user.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(@InjectRepository(User) private userRepository: UsersRepository) {
+  constructor(
+    @InjectRepository(User) private userRepository: UsersRepository,
+    private configService: ConfigService,
+  ) {
     super({
-      secretOrKey: 'secret',
+      secretOrKey: configService.get('JWT_SECRET'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
